acb57edef55584e26e221f32c34953a3e923a59a
chrieric <chrieric@onid.oregonstate.edu>
2015-02-15 15:33:20 -0600

    Q 2
    Updated queries and front page. Attempting to link functions with data
    sent via post. Not sure how successful at the moment. Beginning testing
    in next iteration.

--- QueryFunctions.php
+++ QueryFunctions.php
@@ -5,24 +5,21 @@ error_reporting(E_ALL);
 ini_set('display_errors',1);
 header('Content-Type: text/plain');
 
-
-
+$failure=0;
  
 $mysqli=new mysqli('oniddb.cws.oregonstate.edu','chrieric-db','temp','chrieric-db');
 if(!$mysqli||$mysqli->connect_errno)
 {
   echo 'Connection error' . $mysqli->connect_errno . '' . $mysqli->connect_error;
 } 
+
  
 function addMovie(array)
-{
-  
-  $name=$_POST["name"];
-  $category=$_POST["category"];
-  $length=$_POST["length"];
+{ 
+  $name=array["name"];
+  $category=array["category"];
+  $length=array["length"];
     
-    if($_POST != null)
-    {
         if($name !=null && $category != null && $length != null)
         {
           /*this is where you add functionality to add the data to the table via SQL call
@@ -62,47 +59,36 @@ function addMovie(array)
           {
             echo 'You failed to enter a movie length';
           }
+		  
+		  echo "Error while adding video, click <a href='front_page.php'>here</a> to return to your inventory.";
         }
-    }
-    else
-    {
-        echo 'There was an error submitting your data';
     }
 };
  
 function deleteMovie(array)
 {
-  $id=$_POST['button_id'] /*this is just to inform me what should be here, change later*/
+  $id=$_POST['id'] /*this is just to inform me what should be here, change later*/
   
-  if($name!=null)
-  {
-    if(!$stmt=$mysqli->prepare("DELETE FROM video_store_inventory WHERE (id = ?)"))
-   {
-      echo 'Prep failed: (' . $mysqli->errno . ') ' . $mysqli->error
+	if($name!=null)
+	{
+		if(!$stmt=$mysqli->prepare("DELETE FROM video_store_inventory WHERE (id = ?)"))
+		{
+			echo 'Prep failed: (' . $mysqli->errno . ') ' . $mysqli->error
+		}
+		elseif(!$stmt->bind_param("s", $id))
+		{
+			echo 'Binding failed: (' . $stmt->errno . ') ' . $stmt->error;
+		}
+		else if(!stmt->execute())
+		{
+			echo 'Execution failed: ' . $stmt->errno . ') ' . $stmt->error;
+		}
+		$stmt->close();
+	}
+	else
+	{
+		echo "Name not found click <a href='front_page.php'>here</a> to return to your inventory.";
     }
-    elseif(!$stmt->bind_param("i", $id))
-    {
-      echo 'Binding failed: (' . $stmt->errno . ') ' . $stmt->error;
-    }
-    else if(!stmt->execute())
-    {
-      echo 'Execution failed: ' . $stmt->errno . ') ' . $stmt->error;
-    }
-    else
-    {
-        mysqli_stmt::bind_param('i',$id);
-        $stmt->execute();
-    }
-    $stmt->close();
-    }
-    else
-    {
-      if($name==null)
-      {
-        echo 'ID not found';
-      }
-    }
-  }
 };
  
 function deleteAll(array)
@@ -115,52 +101,88 @@ function deleteAll(array)
 	{
 		echo 'Execution failed: ' . $stmt->errno . ') ' . $stmt->error;
     }
-    else
-    {
-        mysqli_stmt::bind_param('i',$id);
-        $stmt->execute();
-    }
     $stmt->close();
 }
 
 function checkInOut(array)
 {
-	if(!$stmt=$mysqli->prepare("UPDATE video_store_inventory SET rented=0 WHERE name=?"))
+	//gets id passed from post
+	$id=array['id'];
+	//temp variable
+	$temp_rented;
+	
+	//checks to deteremine if an errors
+	if(!(stmt = $mysqli->query("SELECT rented FROM video_store_inventory WHERE id=?"))
 	{
-		echo 'Prep failed: (' . $mysqli->errno . ') ' . $mysqli->error
+		echo 'Query failed: (' . $mysqli->errno . ') ' . $mysqli->error;
 	}
-	elseif(elseif(!$stmt->bind_param("s", $id))
+	//if no errors, fetches value of rented
+	//then changes rented value from 0 to 1 or 1 to 0
+	else
+	{	stmt = $mysqli->query("SELECT rented FROM video_store_inventory WHERE name=?"
+		$temp_rented=stmt->fetch_assoc();
+		
+		if($temp_rented==0)
+		{
+			if(!($stmt=$mysqli->prepare("UPDATE video_store_inventory SET rented=1 WHERE id=?")))
+			{
+				echo 'Prep failed: (' . $mysqli->errno . ') ' . $mysqli->error;
+			}
+			elseif((!($stmt->bind_param("i", $id)))
+			{
+				echo 'Execution failed: ' . $stmt->errno . ') ' . $stmt->error;
+			}
+			else if(!(stmt->execute()))
+			{
+				echo 'Execution failed: ' . $stmt->errno . ') ' . $stmt->error;
+			}
+		}
+		else
+		{
+			if(!($stmt=$mysqli->prepare("UPDATE video_store_inventory SET rented=0 WHERE name=?")))
+			{
+				echo 'Prep failed: (' . $mysqli->errno . ') ' . $mysqli->error;
+			}
+			elseif(!($stmt->bind_param("i", $id)))
+			{
+				echo 'Execution failed: ' . $stmt->errno . ') ' . $stmt->error;
+			}
+			else if(!(stmt->execute()))
+			{
+				echo 'Execution failed: ' . $stmt->errno . ') ' . $stmt->error;
+			}
+		}
+	}
+	$stmt->close();
+}
+
+if($_POST != null)
+{
+	if(isset($_POST['add_video']))
 	{
-		echo 'Execution failed: ' . $stmt->errno . ') ' . $stmt->error;
+		addMovie($_POST);
 	}
-    else if(!stmt->execute())
-    {
-      echo 'Execution failed: ' . $stmt->errno . ') ' . $stmt->error;
-    }
-    else
-    {
-        mysqli_stmt::bind_param('i',$id);
-        $stmt->execute();
-    }
-    $stmt->close();
+	elseif(isset($_POST['delete_movie']))
+	{
+		deleteMovie($_POST);
+	}
+	elseif(isset($_POST['delete_all']))
+	{
+		deleteAll($_POST);
+	}
+	elseif(isset($_POST['check_in_out']))
+	{
+		checkInOut($_POST);
+	}
+
+	if($failure==0)
+	{
+		header("Location:front_page.php",true);
+	}
 }
- 
- 
-if(isset($_POST['add_video']))
+else
 {
-	addMovie($_POST);
-}
-elseif(isset($_POST['delete_movie']))
-{
-	deleteMovie($_POST);
-}
-elseif(isset($_POST['delete_all']))
-{
-	deleteAll($_POST);
-}
-elseif(isset($_POST['check_in_out']))
-{
-	checkInOut($_POST);
+	echo "Error while sending data, click <a href='front_page.php'>here</a> to return to your inventory.";
 }
  
 ?>
--- front_page.php
+++ front_page.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+ob_start();
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+header('Content-Type: text/plain');
+
+$mysqli=new mysqli('oniddb.cws.oregonstate.edu','chrieric-db','temp','chrieric-db');
+if(!$mysqli||$mysqli->connect_errno)
+{
+  echo 'Connection error ' . $mysqli->connect_errno . '' . $mysqli->connect_error;
+} 
+
+?>	
+
 <!DOCTYPE html>
 <html lang='en'>
 <meta charset='utf-8'>
@@ -6,13 +21,7 @@
 </head>
 <body>
     <section>
-<?php
-session_start();
-ob_start();
-error_reporting(E_ALL);
-ini_set('display_errors',1);
-header('Content-Type: text/plain');
-?>	
+
     <form action='QueryFunctions.php' method='post'>
         <input type='button' name='delete_all' value='Delete All'>
 		<input type='text' name='name'>
@@ -20,8 +29,48 @@ header('Content-Type: text/plain');
         <input type='text' name='length'>
         <input type='submit' name='add_video' value='Add'>
     </form>
+	
+	
+	<table border='0.5'>
+	<thead>
+	<tr>
+		<th>Title</th>
+		<th>Category</th>
+		<th>Length</th>
+		<th>Rented</th>
+		<th>Check-In/Out</th>
+		<th>Delete Movie</th>
+	</tr>
+	</thead>
+	<tbody>
 <?php
+	$row;
+	
+	while($row=mysqli_fetch_array($stmt))
+	{
+		echo "<tr>";
+		echo "<td>" . $row['name'] . "</td>";
+		echo "<td>" . $row['category'] . "</td>";
+		if($row['length']==0)
+		{
+			echo "<td>" . "none listed" . "</td>";
+		}
+		else
+		{
+			echo "<td>" . $row['length'] . "</td>";
 
+		}
+		echo "<td>" . $row['rented'] . "</td>";
+		echo "<td>";
+		echo "<form action='QueryFunctions.php' method='post'>";
+		echo "<input type='submit' name='check_in_out' value='Check In/Out' id='$row['id']'>";
+		echo "</td>";
+		
+		echo "<td>";
+		echo "<form action='QueryFunctions.php' method='post'>";
+		echo "<input type='submit' name='delete_movie' value='delete' id='$row['id']'>";
+		echo "</td>";
+	}
 ?>
 	
     </section>
